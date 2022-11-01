080c7b550e20f43e6c4a4c8b6413f6a3b5d4d6f3
王超 <wangchao2423@gmail.com>
2022-11-01 13:38:33 +0800

    日常更新1

--- 02_复习 TypeScript/04_TypeScript 函数类型/03_构造签名.ts
+++ 02_复习 TypeScript/04_TypeScript 函数类型/03_构造签名.ts
@@ -4,7 +4,7 @@
  * @Author: wc
  * @Date: 2022-10-24 17:06:30
  * @LastEditors: wc
- * @LastEditTime: 2022-10-24 17:10:35
+ * @LastEditTime: 2022-10-27 16:27:32
  */
 
 class Person {}
@@ -16,3 +16,25 @@ interface IType {
 function f(fn: IType) {
   const a = new fn();
 }
+
+interface IPerson {
+  name: string;
+  age: number;
+}
+
+const a = {
+  name: "itchao",
+  age: 23,
+  height: 20
+};
+
+const info: IPerson = a;
+
+// interface IPType {
+//   [index: number]: string;
+// }
+
+// const i: IPType = ["1", "2", "3"];
+// i.forEach(() => {});
+
+export {};
