410be74d4ee6927265509635aaa86f8dfd978608
王超 <wangchao2423@gmail.com>
2022-11-04 11:07:24 +0800

    feat(TypeScript 知识扩展): 补充部分内容

---
+++ 02_复习 TypeScript/08_TypeScript 知识扩展-webpack/src/index.ts
@@ -0,0 +1,11 @@
+/*
+ * @Desc:
+ * @Version: v1.00
+ * @Author: wc
+ * @Date: 2022-11-03 14:15:03
+ * @LastEditors: wc
+ * @LastEditTime: 2022-11-03 16:33:06
+ */
+const a: number = 1;
+
+console.log(a);
