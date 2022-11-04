3b8dc1ded367d6275384d6bcea777a7d057ecb51
王超 <wangchao2423@gmail.com>
2022-11-04 16:01:18 +0800

    feat(TypeScript 知识补充): webpack 结合 TS

--- 02_复习 TypeScript/08_TypeScript 知识扩展-webpack/src/index.ts
+++ 02_复习 TypeScript/08_TypeScript 知识扩展-webpack/src/index.ts
@@ -4,8 +4,18 @@
  * @Author: wc
  * @Date: 2022-11-03 14:15:03
  * @LastEditors: wc
- * @LastEditTime: 2022-11-03 16:33:06
+ * @LastEditTime: 2022-11-04 14:27:57
  */
-const a: number = 1;
+interface IPeople {
+  name: string;
+  age: number;
+  height: string;
+}
 
-console.log(a);
+const people: IPeople = {
+  name: "itchao",
+  age: 23,
+  height: "1.88"
+};
+
+console.log(people);
