6559e63cf85bba0037a8332d97852cd4a46c368b
王超 <wangchao2423@gmail.com>
2022-11-07 16:48:51 +0800

    feat(TypeScript 类型体操): 类型体操练习

--- 02_复习 TypeScript/09_TypeScript 知识扩展-axios 封装/src/index.ts
+++ 02_复习 TypeScript/09_TypeScript 知识扩展-axios 封装/src/index.ts
@@ -4,7 +4,7 @@
  * @Author: wc
  * @Date: 2022-11-03 14:15:03
  * @LastEditors: wc
- * @LastEditTime: 2022-11-07 11:23:30
+ * @LastEditTime: 2022-11-07 14:39:58
  */
 
 import "./service/modules/home";
---
+++ 02_复习 TypeScript/10_TypeScript 条件类型-类型体操/01_TS条件类型基本使用.ts
@@ -0,0 +1,8 @@
+/*
+ * @Desc:
+ * @Version: v1.00
+ * @Author: wc
+ * @Date: 2022-11-07 16:30:00
+ * @LastEditors: wc
+ * @LastEditTime: 2022-11-07 16:30:01
+ */
